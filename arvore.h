77998c70ce407528ba2386523b62ef1ae5cc108c
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-07 14:32:45 -0300

    'Adição da funções do projeto'

--- arvore.h
+++ arvore.h
@@ -10,10 +10,22 @@ typedef struct _arvore {
 } Arvore ;
 
 void iniciarArvore(Arvore **);
-
-
-
-
-
+void inserirNo(Arvore **, int);
+void lerArquivo(Arvore **);
+void lerDoUsuario(Arvore **);
+void printarNoRaiz(Arvore *);
+void printarNosFolha(Arvore *);
+void printarNosRamo(Arvore *);
+int alturaDaArvore(Arvore *);
+int profundidadeDaArvore(Arvore *);
+int grauDoNo(Arvore *);
+int alturaDoNo(Arvore *);
+int profundidoDoNo(Arvore *);
+void printarDescendentes(Arvore *);
+void printarAncestrais(Arvore *);
+void preOrdem(Arvore *);
+void posOrdem(Arvore *);
+void emOrdem(Arvore *);
+Arvore* buscar(Arvore *, int);
 
 #endif
