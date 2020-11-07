78ac57907d503689fbee1d4ca2d47a827e043b4e
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-07 16:58:55 -0300

    'Inclusao do stdio.h, implementação menu interativo e reduzação de linhas não utlizadas'

--- func.c
+++ func.c
@@ -1,87 +1,63 @@
+#include <stdio.h>
 #include "arvore.h"
 
 #define true 1
 #define false 0
 
-
 void iniciarArvore(Arvore **tree) { // Inicializa a arvore com valor "vazio"
-
     *tree = NULL;
 }
 
-void inserirNo(Arvore **tree, int valor) {
-    
-}
+int printarMenu() {
+    int valor;
 
-void lerArquivo(Arvore **tree) {
+    printf(
+        "\n============== Menu =================\n"
+        "1) Digitar as entradas\n"
+        "2) Ler do arquivo\n"
+        "3) Encerrar\n"
+        "\n"
+        "Digite a opcao desejada: "
+        );
+  
+    scanf("%d", &valor); 
 
-}
-
-void lerDoUsuario(Arvore **tree) {
-
-}
-
-void printarMenu() {
-    
-}
-
-int estaVazia(Arvore *tree) {
-
-    return false;
-}
-
-Arvore* buscar(Arvore *tree,int valor) {
+    return valor;
 
 }
 
-void printarNoRaiz(Arvore *tree) {
+void inserirNo(Arvore **tree, int valor) {}
 
-}
+void lerArquivo(Arvore **tree) {}
 
-void printarNosFolha(Arvore *tree) {
+void lerDoUsuario(Arvore **tree) {}
 
-}
+int estaVazia(Arvore *tree) {}
 
-void printarNosRamo(Arvore *tree) {
+Arvore* buscar(Arvore *tree,int valor) {}
 
-}
+void printarNoRaiz(Arvore *tree) {}
 
-int alturaDaArvore(Arvore *tree) {
+void printarNosFolha(Arvore *tree) {}
 
-}
+void printarNosRamo(Arvore *tree) {}
 
-int profundidadeDaArvore(Arvore *tree) {
+int alturaDaArvore(Arvore *tree) {}
 
-}
+int profundidadeDaArvore(Arvore *tree) {}
 
-int grauDoNo(Arvore *tree) {
+int grauDoNo(Arvore *tree) {}
 
-}
+int alturaDoNo(Arvore *tree) {}
 
-int alturaDoNo(Arvore *tree) {
+int profundidoDoNo(Arvore *tree) {}
 
-}
+void printarDescendentes(Arvore *tree) {}
 
-int profundidoDoNo(Arvore *tree) {
+void printarAncestrais(Arvore *tree) {}
 
-}
+void preOrdem(Arvore *tree) {} //Gui
 
-void printarDescendentes(Arvore *tree) {
+void posOrdem(Arvore *tree) {} //Gui
 
-}
-
-void printarAncestrais(Arvore *tree) {
-
-}
-
-void preOrdem(Arvore *tree) { // Gui 
-
-}
-
-void posOrdem(Arvore *tree) { // Gui 
-
-}
-
-void emOrdem(Arvore *tree) { // Gui
-
-}
+void emOrdem(Arvore *tree) {} //Gui
