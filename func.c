ba37a30a8a937fc4aa9de1c952f79f7c0c5d44ee
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-06 21:22:46 -0300

    'Adição de métodos que poderam ser implementados'

--- func.c
+++ func.c
@@ -1,2 +1,49 @@
 #include "arvore.h"
 
+void iniciarArvore() {
+
+}
+
+void adicionarNo() {
+    
+}
+
+Arvore* buscar() {
+
+}
+
+void printarNoRaiz() {
+
+}
+
+void printarNosFolha() {
+
+}
+
+void printarNosRamo() {
+
+}
+
+void alturaProfundArvore() {
+
+}
+
+void grauAlturaProfundidadeNo() {
+
+}
+
+void printarAncestraisEDescendentes() {
+
+}
+
+void preOrdem() {
+
+}
+
+void posOrdem() {
+
+}
+
+void emOrdem() {
+
+}
